a52c2376461b439607c066b3d8670ab7b05c85d6
Pavithra Elangovan <113089772+Pavithra0706@users.noreply.github.com>
2023-09-25 20:35:37 +0530

    Update code2.js

--- anonymous and IIFE/code2.js
+++ anonymous and IIFE/code2.js
@@ -1,1 +1,15 @@
+/ Do the below programs in anonymous function & IIFE 
+// 2) Sum of all numbers in an array. 
 
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = function(arr) {
+    return arr.reduce(function(total, num) {
+      return total + num;
+    }, 0);
+  };
+  
+  console.log(sum(numbers));
+
+  //------------------------------
