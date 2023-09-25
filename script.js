294177d09116d0aa5a2125f93a98a39982667d11
Pavithra Elangovan <113089772+Pavithra0706@users.noreply.github.com>
2023-09-25 19:24:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//  Do the below programs in anonymous function & IIFE
+
+//  1)Print odd numbers in an array using anonymous function
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const printOddNumbers = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 !== 0) {
+        console.log(arr[i]);
+      }
+    }
+  };
+  
+  printOddNumbers(numbers);
+   
+  
+//-------------------------------------------------------------------------------------------------------------------------------
+// using IIFE
+
+  const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+(function(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      console.log(arr[i]);
+    }
+  }
+})(numbers1);
