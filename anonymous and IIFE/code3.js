a6fe2c56c3b246b7f3db96f58b09e22e7f332d7d
Pavithra Elangovan <113089772+Pavithra0706@users.noreply.github.com>
2023-09-25 20:36:36 +0530

    Add files via upload

---
+++ anonymous and IIFE/code3.js
@@ -0,0 +1,26 @@
+// Do the below programs in anonymous function & IIFE 
+// 2) Sum of all numbers in an array. 
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = function(arr) {
+    return arr.reduce(function(total, num) {
+      return total + num;
+    }, 0);
+  };
+  
+  console.log(sum(numbers));
+
+  //--------------------------------------------------------------
+  // Using IIFE 
+
+const numbers1 = [1, 2, 3, 4, 5];
+
+const sum1= (function(arr) {
+  return arr.reduce((total, num) => total + num, 0);
+})(numbers1);
+
+console.log(sum1);
+
+  
