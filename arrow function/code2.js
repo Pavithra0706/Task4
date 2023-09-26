b7987a7eb8a34aa20cd8ec2c19093b47b53ec71f
Pavithra Elangovan <113089772+Pavithra0706@users.noreply.github.com>
2023-09-26 14:48:14 +0530

    Add files via upload

---
+++ arrow function/code2.js
@@ -0,0 +1,11 @@
+//Do the below programs in arrow functions.
+// 2) Convert all the strings to title caps in a string array
+
+let str = ["pavi","is","a","software","developer"];
+
+ let strCaps = str.map((item) => {
+
+    return item.toUpperCase()
+
+})
+console.log( strCaps)
